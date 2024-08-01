b3b937bc573319340b6dd3a9b4c54a6e843ea5d2
musi <musiksd1896@gmail.com>
2024-08-01 10:41:20 +0530

    Added image folder to add them locally

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User'); // Adjust the path as per your project structure
 const jwt = require('jsonwebtoken');
 
