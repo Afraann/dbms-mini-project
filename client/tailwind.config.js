aa5d3a1db0c684f898a2b230538f4461ab91043c
ASUS <zulfamariyam2727@gmail.com>
2024-08-01 11:12:58 +0530

    Added tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}',],
+  theme: {
+    extend: {
+      colors: {
+        'beige-100': '#F5F5DC',
+        'beige-300': '#E5E5C0',
+        'brown-600': '#8B4513',
+        'brown-700': '#6B4423',
+      },
+    },
+  },
+  plugins: [],
+}
+
